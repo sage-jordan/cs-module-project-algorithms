a8065db3cfd20deb7c6fae1eb30a1ca77bd8f077
sage-jordan <sagemjordan@gmail.com>
2020-06-09 21:11:34 -0400

    working on moving_zeroes, stuck (noted)

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,10 +3,23 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
-
+    # If empty
+    if len(arr) <= 1:
+        return arr
+    
+    # Loop over arr
+    for i in range(0, len(arr) - 1):
+    # If current value is 0, .append the popped val
+        if arr[i] == 0:
+            popped = arr.pop(i)
+            arr.append(popped)
+            print('\n')
+            print(popped)
+        
+        
+    # after loop, return arr
+    print(arr)
+    return arr ### err: returns after merging all 0s in last test
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
